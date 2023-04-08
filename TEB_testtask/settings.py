73d8bbcda48bff4e841a716434126262aeaf9a33
Nikita Zemtsov <zemtsovnikig@gmail.com>
2023-04-08 23:25:37 +0300

    Fix db

--- TEB_testtask/settings.py
+++ TEB_testtask/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'TEB_testtask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'uIDVu1nnAO9jt2lRbNxN',
+        'HOST': 'containers-us-west-14.railway.app',
+        'PORT': '5788',
     }
 }
 
