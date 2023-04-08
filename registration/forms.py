9742676f1d9c20ec6dd17ffcadaefb8807ba0687
Nikita Zemtsov <zemtsovnikig@gmail.com>
2023-04-08 20:26:05 +0300

    Fix CSRF bug

--- registration/forms.py
+++ registration/forms.py
@@ -1,10 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
-
-
-from .models import *
-
 
 
 class LoginUserForm(AuthenticationForm):
